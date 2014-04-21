e102d83bcfa685c2a7c4828fb5912135a8789677
Sébastien Lavoie <github@lavoie.sl>
2014-04-21 14:52:30 -0400

    init

---
+++ src/Composer/ConfHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WMC\Wordpress\SkeletonInstaller\Composer;
+
+use Composer\Script\Event;
+use WMC\Composer\Utils\ConfigFile\IniConfigFile;
+
+class ConfHandler
+{
+    public static function updateFiles(Event $event)
+    {
+        $configFile = new IniConfigFile($event->getIO());
+
+        foreach (glob(getcwd() . '/confs/samples/*.ini') as $file) {
+            $file = basename($file);
+            $configFile->updateFile(getcwd() . "/confs/$file", getcwd() . "/confs/samples/$file");
+        }
+    }
+}
